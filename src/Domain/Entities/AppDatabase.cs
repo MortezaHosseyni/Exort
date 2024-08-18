dc9d0cafa1f95a8fd9ca09195ea8e4b3e78179aa
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2024-08-18 11:08:12 +0330

    AppDatabase Create AppDatabase and define entities in this class Use MongoDB driver for database connection and operations Create Community repository (example)

---
+++ src/Domain/Entities/AppDatabase.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+
+namespace Domain.Entities
+{
+    public class AppDatabase
+    {
+        private readonly IMongoDatabase _database;
+
+        public AppDatabase(IConfiguration configuration)
+        {
+            // MongoDB connection setting
+            var connectionString = configuration.GetConnectionString("MongoDb");
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(configuration["DatabaseName"]);
+        }
+
+        // User collections
+        public IMongoCollection<User> U =>
+            _database.GetCollection<User>("Users");
+
+        #region Community
+        // Community collections
+        public IMongoCollection<Community> C =>
+            _database.GetCollection<Community>("Communities");
+
+        // Community Part collections
+        public IMongoCollection<CommunityPart> CP =>
+            _database.GetCollection<CommunityPart>("CommunityParts");
+
+        // Community Channel collections
+        public IMongoCollection<CommunityChannel> CC =>
+            _database.GetCollection<CommunityChannel>("CommunityChannels");
+
+        // Community Message collections
+        public IMongoCollection<CommunityMessage> CM =>
+            _database.GetCollection<CommunityMessage>("CommunityMessages");
+
+        // Users Community collections
+        public IMongoCollection<UsersCommunity> UC =>
+            _database.GetCollection<UsersCommunity>("UsersCommunities");
+        #endregion
+
+        #region Friends
+        // Friends collections
+        public IMongoCollection<Friend> F =>
+            _database.GetCollection<Friend>("Friends");
+
+        // Private Messages collections
+        public IMongoCollection<PrivateMessage> PM =>
+            _database.GetCollection<PrivateMessage>("PrivateMessages");
+        #endregion
+    }
+}
---
+++ src/Domain/Repositories/CommunityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface ICommunityRepository
+    {
+        Task<Community> Add(Community community);
+    }
+    public class CommunityRepository : ICommunityRepository
+    {
+        private readonly AppDatabase _collection;
+
+        public CommunityRepository(AppDatabase context)
+        {
+            _collection = context;
+        }
+
+
+        public async Task<Community> Add(Community community)
+        {
+            if (community == null) throw new ArgumentNullException("Information is invalid.");
+
+            await _collection.C.InsertOneAsync(community);
+
+            return community;
+        }
+    }
+}
